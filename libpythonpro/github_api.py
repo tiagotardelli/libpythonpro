4c86b82b2ac8b24a8586cdcd203524c4e798293e
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-07-08 22:46:05 -0300

    Criado packge com modulo de consulta via requests

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,20 @@
+import requests as requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca do avatar de um usuário no Github
+
+    :param usuario: str com o nome de usuário no githhub
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('tiagotardelli'))
+
+
+
+
