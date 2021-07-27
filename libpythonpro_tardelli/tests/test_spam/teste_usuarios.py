89db9905458229f9d725c51ca904bb3def34eac9
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-07-27 05:14:41 -0300

    Alterações nos testes + mock manual

--- libpythonpro_tardelli/tests/test_spam/teste_usuarios.py
+++ libpythonpro_tardelli/tests/test_spam/teste_usuarios.py
@@ -1,35 +1,18 @@
-import pytest
-
 from libpythonpro_tardelli.spam.modelos import Usuario
-from libpythonpro_tardelli.spam.db import Conexao
-
-
-@pytest.fixture(scope='session')
-def conexao():
-    # Setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(conexao, sessao):
-    usuario = Usuario(nome='Tiago')
+    usuario = Usuario(nome='Tiago', email='contato@tiagotardelli.com.br')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(conexao, sessao):
     sessao = conexao.gerar_sessao()
-    usuarios = [Usuario(nome='Tiago'), Usuario(nome='Marcos')]
+    usuarios = [
+        Usuario(nome='Tiago', email='contato@tiagotardelli.com.br'),
+        Usuario(nome='Marcos', email='contato@tiagotardelli.com.br')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
