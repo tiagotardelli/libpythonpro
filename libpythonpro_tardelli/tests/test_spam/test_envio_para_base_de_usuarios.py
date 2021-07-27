44847c29b73e2f132492d4e5f6e5e049b650d363
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-07-27 08:02:25 -0300

    Adicionando Mock do unittest

--- libpythonpro_tardelli/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_tardelli/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,19 +1,9 @@
+from unittest.mock import Mock
+
 import pytest
 
-from libpythonpro_tardelli.spam.enviador_de_email import Enviador
 from libpythonpro_tardelli.spam.main import EnviadorDeSpam
 from libpythonpro_tardelli.spam.modelos import Usuario
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -31,27 +21,27 @@ class EnviadorMock(Enviador):
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'contato@tiagotardelli.com.br',
         'Cursos Python Pro',
         'Confira os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Tiago', email='contato@tiagotardelli.com.br')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'tiagob.tardelli@gmail.com.br',
         'Cursos Python Pro',
         'Confira os módulos fantásticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with == (
         'tiagob.tardelli@gmail.com.br',
         'contato@tiagotardelli.com.br',
         'Cursos Python Pro',
