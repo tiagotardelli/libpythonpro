1ff5cdc52c930ee49710076063e685e518c4cc29
Tiago Tardelli <tiagob.tardelli@gmail.com>
2021-07-27 21:42:00 -0300

    Adicionando biblioteca PyTest-Mock

--- libpythonpro_tardelli/tests/test_spam/test_github_api.py
+++ libpythonpro_tardelli/tests/test_spam/test_github_api.py
@@ -6,17 +6,16 @@ from libpythonpro_tardelli import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/694103?v=4'
     resp_mock.json.return_value = {
         'login': 'tiagotardelli', 'id': 694103, 'node_id': 'MDQ6VXNlcjY5NDEwMw==',
         'avatar_url': url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro_tardelli.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
